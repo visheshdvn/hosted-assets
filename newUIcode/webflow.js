b3e2a622fcd7cbbdaea7769ecd5c7fc97b9b20ce
Vishesh Dhawan <vishesh.dh1@gmail.com>
2021-12-29 04:08:02 +0530

    mods

--- newUIcode/webflow.js
+++ newUIcode/webflow.js
@@ -4,7 +4,7 @@ window.addEventListener("load", async function () {
     .addEventListener("click", connectToEth);
 
   document.getElementById("claimICT").addEventListener("click", claimICT);
-  document.getElementById("getSkinsBtn").addEventListener("click", getNFTData);
+//   document.getElementById("getSkinsBtn").addEventListener("click", getNFTData);
 });
 
 // connect to metamask
@@ -22,7 +22,7 @@ async function connectToEth() {
   console.log(`Wallet address: ${accounts[0]}`);
 
   // total supply
-  getTotalSupply();
+//   getTotalSupply();
 }
 
 async function getTotalSupply() {
@@ -46,54 +46,54 @@ async function getTotalSupply() {
 }
 
 //get contract instance
-async function getContractInstances() {
-  let { Web3 } = window;
-  const web3 = new Web3(window.ethereum);
-  try {
-    let LedNFT_ABI, ICT_ABI;
+// async function getContractInstances() {
+//   let { Web3 } = window;
+//   const web3 = new Web3(window.ethereum);
+//   try {
+//     let LedNFT_ABI, ICT_ABI;
 
-    let ledNftFileResponse = await fetch(
-      "https://raw.githubusercontent.com/visheshdvn/hosted-assets/main/webflow_goreli_ABIs/LedNFT.json"
-    );
-    LedNFT_ABI = await ledNftFileResponse.json();
+//     let ledNftFileResponse = await fetch(
+//       "https://raw.githubusercontent.com/visheshdvn/hosted-assets/main/webflow_goreli_ABIs/LedNFT.json"
+//     );
+//     LedNFT_ABI = await ledNftFileResponse.json();
 
-    let ICTFileResponse = await fetch(
-      "https://raw.githubusercontent.com/visheshdvn/hosted-assets/main/webflow_goreli_ABIs/IntensityChangeToken.json"
-    );
-    ICT_ABI = await ICTFileResponse.json();
+//     let ICTFileResponse = await fetch(
+//       "https://raw.githubusercontent.com/visheshdvn/hosted-assets/main/webflow_goreli_ABIs/IntensityChangeToken.json"
+//     );
+//     ICT_ABI = await ICTFileResponse.json();
 
-    // let web3 = await getWeb3();
-    const id = await web3.eth.net.getId();
+//     // let web3 = await getWeb3();
+//     const id = await web3.eth.net.getId();
 
-    const ledNFTContractAddress = LedNFT_ABI.networks[id].address;
-    const ledNFTContractInstance = new web3.eth.Contract(
-      LedNFT_ABI.abi,
-      ledNFTContractAddress
-    );
+//     const ledNFTContractAddress = LedNFT_ABI.networks[id].address;
+//     const ledNFTContractInstance = new web3.eth.Contract(
+//       LedNFT_ABI.abi,
+//       ledNFTContractAddress
+//     );
 
-    const ICTContractAddress = ICT_ABI.networks[id].address;
-    const ICTContractInstance = new web3.eth.Contract(
-      ICT_ABI.abi,
-      ICTContractAddress
-    );
+//     const ICTContractAddress = ICT_ABI.networks[id].address;
+//     const ICTContractInstance = new web3.eth.Contract(
+//       ICT_ABI.abi,
+//       ICTContractAddress
+//     );
 
-    return Promise.resolve({
-      // web3,
-      ledNFTContractInstance,
-      ICTContractInstance,
-    });
-  } catch (error) {
-    console.error("Error: ", error, {
-      METHOD: "getWeb3AndContractInstances",
-      FILE: "index.js",
-    });
-    console.error("Error: ", error, {
-      METHOD: "getContractInstance()",
-      FILE: "index.js",
-    });
-    // throw error;
-  }
-}
+//     return Promise.resolve({
+//       // web3,
+//       ledNFTContractInstance,
+//       ICTContractInstance,
+//     });
+//   } catch (error) {
+//     console.error("Error: ", error, {
+//       METHOD: "getWeb3AndContractInstances",
+//       FILE: "index.js",
+//     });
+//     console.error("Error: ", error, {
+//       METHOD: "getContractInstance()",
+//       FILE: "index.js",
+//     });
+//     // throw error;
+//   }
+// }
 
 async function claimICT() {
   let { Web3 } = window;
@@ -163,7 +163,9 @@ async function claimICT() {
     );
     document.getElementById("ictoutput").innerHTML =
       "ICT (Pulse Tokens) <br/>" + addressICTBalanceInEth + ` ICT`;
-    console.log("claimed " + addressICTBalanceInEth + ` ICT`);
+
+      console.log("claimed " + addressICTBalanceInEth + ` ICT`);
+      document.getElementById("ictMessage").innerText = addressICTBalanceInEth;
   } catch (error) {
     console.error("Error: ", error, {
       METHOD: "getData()",
@@ -174,129 +176,129 @@ async function claimICT() {
 }
 
 // get NFT data
-async function getNFTData() {
-  let { Web3 } = window;
-  const web3 = new Web3(window.ethereum);
-  try {
-    if ((await web3.eth.net.getNetworkType()) !== "goerli") {
-      window.alert("Connect to goerli network");
-      throw new Error("Connect to Goerli network");
-    }
-    let { ledNFTContractInstance } = await getContractInstances();
-    let accounts = await web3.eth.getAccounts();
+// async function getNFTData() {
+//   let { Web3 } = window;
+//   const web3 = new Web3(window.ethereum);
+//   try {
+//     if ((await web3.eth.net.getNetworkType()) !== "goerli") {
+//       window.alert("Connect to goerli network");
+//       throw new Error("Connect to Goerli network");
+//     }
+//     let { ledNFTContractInstance } = await getContractInstances();
+//     let accounts = await web3.eth.getAccounts();
 
-    // let contractInstance = ledNFTContractInstance;
-    let balanceOfNFTForThisAddress = await ledNFTContractInstance.methods
-      .balanceOf(accounts[0])
-      .call();
+//     // let contractInstance = ledNFTContractInstance;
+//     let balanceOfNFTForThisAddress = await ledNFTContractInstance.methods
+//       .balanceOf(accounts[0])
+//       .call();
 
-    let tokenId;
-    if (balanceOfNFTForThisAddress >= 1) {
-      tokenId = await ledNFTContractInstance.methods
-        .tokenOfOwnerByIndex(accounts[0], 0)
-        .call();
-    } else {
-      throw new Error("You don't have any tokens");
-      return;
-    }
+//     let tokenId;
+//     if (balanceOfNFTForThisAddress >= 1) {
+//       tokenId = await ledNFTContractInstance.methods
+//         .tokenOfOwnerByIndex(accounts[0], 0)
+//         .call();
+//     } else {
+//       throw new Error("You don't have any tokens");
+//       return;
+//     }
 
-    const res = await ledNFTContractInstance.methods
-      .getSkinsOfToken(tokenId)
-      .call({ from: accounts[0] });
+//     const res = await ledNFTContractInstance.methods
+//       .getSkinsOfToken(tokenId)
+//       .call({ from: accounts[0] });
 
-    let result = "";
-    for (let skin in res) {
-      result += `<li>${res[skin]}</li>`;
-    }
-    result = `<ol> ${result} </ol>`;
-    document.getElementById("skinsoutput").innerHTML = result;
-  } catch (err) {
-    console.error("Error: ", err, {
-      METHOD: "markForSale()",
-      FILE: "index.js",
-    });
-  }
-}
+//     let result = "";
+//     for (let skin in res) {
+//       result += `<li>${res[skin]}</li>`;
+//     }
+//     result = `<ol> ${result} </ol>`;
+//     document.getElementById("skinsoutput").innerHTML = result;
+//   } catch (err) {
+//     console.error("Error: ", err, {
+//       METHOD: "markForSale()",
+//       FILE: "index.js",
+//     });
+//   }
+// }
 
-formElem.onsubmit = async (e) => {
-  e.preventDefault();
-  console.log("Here1");
-  let { Web3 } = window;
-  const web3 = new Web3(window.ethereum);
-  const formElem = document.getElementById("formElem");
-  try {
-    if ((await web3.eth.net.getNetworkType()) !== "goerli") {
-      window.alert("Connect to goerli network");
-      throw new Error("Connect to Goerli network");
-    }
-    let { ledNFTContractInstance, ICTContractInstance } =
-      await getContractInstances();
-    let accounts = await web3.eth.getAccounts();
+// formElem.onsubmit = async (e) => {
+//   e.preventDefault();
+//   console.log("Here1");
+//   let { Web3 } = window;
+//   const web3 = new Web3(window.ethereum);
+//   const formElem = document.getElementById("formElem");
+//   try {
+//     if ((await web3.eth.net.getNetworkType()) !== "goerli") {
+//       window.alert("Connect to goerli network");
+//       throw new Error("Connect to Goerli network");
+//     }
+//     let { ledNFTContractInstance, ICTContractInstance } =
+//       await getContractInstances();
+//     let accounts = await web3.eth.getAccounts();
 
-    let tokenId = await ledNFTContractInstance.methods
-      .tokenOfOwnerByIndex(accounts[0], 0)
-      .call();
+//     let tokenId = await ledNFTContractInstance.methods
+//       .tokenOfOwnerByIndex(accounts[0], 0)
+//       .call();
 
-    let formData = new FormData(formElem);
-    let blinkPattern = formData.getAll("pattern").map((item) => parseInt(item));
-    let data = {
-      token_id: tokenId,
-      pattern: blinkPattern,
-    };
+//     let formData = new FormData(formElem);
+//     let blinkPattern = formData.getAll("pattern").map((item) => parseInt(item));
+//     let data = {
+//       token_id: tokenId,
+//       pattern: blinkPattern,
+//     };
 
-    try {
-      let addressICTBalanceInWei = await ICTContractInstance.methods
-        .balanceOf(accounts[0])
-        .call();
-      let addressICTBalanceInEth = window.Web3.utils.fromWei(
-        addressICTBalanceInWei,
-        "ether"
-      );
+//     try {
+//       let addressICTBalanceInWei = await ICTContractInstance.methods
+//         .balanceOf(accounts[0])
+//         .call();
+//       let addressICTBalanceInEth = window.Web3.utils.fromWei(
+//         addressICTBalanceInWei,
+//         "ether"
+//       );
 
-      let ownerOfNFT;
-      try {
-        ownerOfNFT = await ledNFTContractInstance.methods
-          .ownerOf(data.token_id)
-          .call();
-      } catch (error) {
-        if (ownerOfNFT != accounts[0]) {
-          console.log("Not authorized");
-          return;
-        }
-      }
+//       let ownerOfNFT;
+//       try {
+//         ownerOfNFT = await ledNFTContractInstance.methods
+//           .ownerOf(data.token_id)
+//           .call();
+//       } catch (error) {
+//         if (ownerOfNFT != accounts[0]) {
+//           console.log("Not authorized");
+//           return;
+//         }
+//       }
 
-      if (ownerOfNFT != accounts[0]) {
-        console.log("Not authorized");
-        return;
-      }
-      console.log("Here2");
-      if (addressICTBalanceInEth < 1) {
-        console.log("Insufficient funds");
-        return;
-      }
-      await ledNFTContractInstance.methods
-        .changeLEDBlinkPattern(data.token_id, data.pattern)
-        .send({ from: accounts[0], gas: 3000000 })
-        .on("error", function (error, receipt) {
-          console.error("Error: ", error, {
-            METHOD: "changeLEDBlinkPattern()",
-            FILE: "index.js",
-          });
-          return;
-        });
-      console.log("Success");
-      return;
-    } catch (error) {
-      console.error("Error: ", error, {
-        METHOD: "changeBlinkPattern()",
-        FILE: "index.js",
-      });
-    }
-  } catch (error) {
-    console.error("Error: ", error, {
-      METHOD: "formElem.onsubmit()",
-      FILE: "index.js",
-    });
-    return;
-  }
-};
+//       if (ownerOfNFT != accounts[0]) {
+//         console.log("Not authorized");
+//         return;
+//       }
+//       console.log("Here2");
+//       if (addressICTBalanceInEth < 1) {
+//         console.log("Insufficient funds");
+//         return;
+//       }
+//       await ledNFTContractInstance.methods
+//         .changeLEDBlinkPattern(data.token_id, data.pattern)
+//         .send({ from: accounts[0], gas: 3000000 })
+//         .on("error", function (error, receipt) {
+//           console.error("Error: ", error, {
+//             METHOD: "changeLEDBlinkPattern()",
+//             FILE: "index.js",
+//           });
+//           return;
+//         });
+//       console.log("Success");
+//       return;
+//     } catch (error) {
+//       console.error("Error: ", error, {
+//         METHOD: "changeBlinkPattern()",
+//         FILE: "index.js",
+//       });
+//     }
+//   } catch (error) {
+//     console.error("Error: ", error, {
+//       METHOD: "formElem.onsubmit()",
+//       FILE: "index.js",
+//     });
+//     return;
+//   }
+// };
